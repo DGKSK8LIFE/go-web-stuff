08e62c427c2c5d51641c9be9dbaf53d7b23cfcd0
Tarek Ali <tarekali15@outlook.com>
2019-09-26 18:11:56 -0500

    Create httpresponsewriter.go

---
+++ httpresponsewriter.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type ResponseRecorder struct {
+	Responses []string
+}
+
+func (r *ResponseRecorder) Get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	r.Responses = append(r.Responses, string(body))
+
+	return body, nil
+}
+
+func main() {
+	recorder := &ResponseRecorder{}
+	recorder.Get("http://www.google.com")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "http response: %v", recorder.Responses)
+	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
+	fmt.Printf("downloaded %d responses\n", len(recorder.Responses))
+}
